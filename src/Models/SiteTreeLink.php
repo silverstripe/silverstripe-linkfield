92b9cd6fb7ebfdace6f11540aea57674f6375df7
Mojmir Fendek <mfendek@silverstripe.com>
2021-11-03 06:54:51 +1300

    PR fixes.

--- src/Models/SiteTreeLink.php
+++ src/Models/SiteTreeLink.php
@@ -7,7 +7,8 @@ use SilverStripe\CMS\Model\SiteTree;
 use SilverStripe\Forms\TreeDropdownField;
 
 /**
- * A link to a Page in the CMS.
+ * A link to a Page in the CMS
+ *
  * @property SiteTree $Page
  * @property int $PageID
  * @property string $Anchor
@@ -74,11 +75,36 @@ class SiteTreeLink extends Link
     {
         parent::onBeforeWrite();
 
-        if ($this->Title || !$this->Page->exists()) {
-            return;
+        $this->populateTitle();
+    }
+
+    protected function populateTitle(): void
+    {
+        $title = $this->getTitleFromPage();
+        $this->extend('updateGetTitleFromPage', $title);
+        $this->Title = $title;
+    }
+
+    /**
+     * Try to populate link title from page title in case we don't have a title yet
+     *
+     * @return string|null
+     */
+    protected function getTitleFromPage(): ?string
+    {
+        if ($this->Title) {
+            // If we already have a title, we can just bail out without any changes
+            return $this->Title;
+        }
+
+        $page = $this->Page;
+
+        if (!$page || !$page->exists()) {
+            // We don't have a page to fall back to
+            return null;
         }
 
         // Use page title as a default value in case CMS user didn't provide the title
-        $this->Title = $this->Page->Title;
+        return $this->Page->Title;
     }
 }
