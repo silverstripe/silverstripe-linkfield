71c50537d1e7299d39105453efbbd68b38e2f94b
Chris Penny <chris.penny@gmail.com>
2023-03-02 07:55:16 +1300

    Enable JS test. Ignore linting. Add sample-test.js

---
+++ client/src/tests/sample-test.js
@@ -0,0 +1,8 @@
+/* global jest, describe, it, expect */
+
+describe('sample tests', () => {
+  it('sample test', () => {
+    const css = 'sample css';
+    expect(css).toBe('sample css');
+  });
+});
