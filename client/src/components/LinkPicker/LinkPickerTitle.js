6f030ee4d2a16bb7de7ec0b504f36e80a0ac52e4
Bernie Hamlin <bernie@silverstripe.com>
2023-03-08 10:25:10 +1300

    remove nested button

--- client/src/components/LinkPicker/LinkPickerTitle.js
+++ client/src/components/LinkPicker/LinkPickerTitle.js
@@ -14,16 +14,18 @@ const stopPropagation = (fn) => (e) => {
 }
 
 const LinkPickerTitle = ({ title, type, description, onClear, onClick }) => (
-  <Button className="link-picker__link font-icon-link" color="secondary" onClick={stopPropagation(onClick)}>
-    <div className="link-picker__link-detail">
+  <div className="link-picker__link" >
+    <Button className="link-picker__button font-icon-link"  color="secondary" onClick={stopPropagation(onClick)}>
+      <div className="link-picker__link-detail">
       <div className="link-picker__title">{title}</div>
       <small className="link-picker__type">
         {type.title}:&nbsp;
         <span className="link-picker__url">{description}</span>
       </small>
-    </div>
+      </div>
+    </Button>
     <Button className="link-picker__clear" color="link" onClick={stopPropagation(onClear)}>{i18n._t('Link.CLEAR', 'Clear')}</Button>
-  </Button>
+  </div>
 );
 
 LinkPickerTitle.propTypes = {
