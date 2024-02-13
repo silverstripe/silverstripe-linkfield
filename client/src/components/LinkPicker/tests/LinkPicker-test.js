2cb85f6c189d11da4d7e0d399418782c1006b1dd
Sabina Talipova <sabina.talipova@silverstripe.com>
2024-02-13 13:21:01 +1300

    MNT LinkField Jest tests

--- client/src/components/LinkPicker/tests/LinkPicker-test.js
+++ client/src/components/LinkPicker/tests/LinkPicker-test.js
@@ -1,6 +1,8 @@
 /* global jest, test */
 import React from 'react';
-import { render, fireEvent, act } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
 import { LinkFieldContext } from 'components/LinkField/LinkField';
 import LinkPicker from '../LinkPicker';
 
@@ -22,6 +24,7 @@ test('LinkPickerMenu render() should display toggle if can create', () => {
     <LinkPicker {...makeProps({ canCreate: true })} />
   </LinkFieldContext.Provider>);
   expect(container.querySelectorAll('.link-picker__menu-toggle')).toHaveLength(1);
+  expect(container.querySelector('.link-picker__menu-toggle')).toHaveTextContent('Add Link');
   expect(container.querySelectorAll('.link-picker__cannot-create')).toHaveLength(0);
 });
 
@@ -31,6 +34,7 @@ test('LinkPickerMenu render() should display cannot create message if cannot cre
   </LinkFieldContext.Provider>);
   expect(container.querySelectorAll('.link-picker__menu-toggle')).toHaveLength(0);
   expect(container.querySelectorAll('.link-picker__cannot-create')).toHaveLength(1);
+  expect(container.querySelector('.link-picker__cannot-create')).toHaveTextContent('Cannot create link');
 });
 
 test('LinkPickerMenu render() should display cannot create message if types is empty', () => {
@@ -60,18 +64,18 @@ test('LinkPickerMenu should open dropdown on click when not loading', async () =
   const { container } = render(<LinkFieldContext.Provider value={{ loading: false }}>
     <LinkPicker {...makeProps()} />
   </LinkFieldContext.Provider>);
-  await act(async () => {
-    await fireEvent.click(container.querySelector('button.link-picker__menu-toggle'));
+  userEvent.click(container.querySelector('button.link-picker__menu-toggle'));
+  await waitFor(() => {
+    expect(container.querySelectorAll('.dropdown-menu.show')).toHaveLength(1);
   });
-  expect(container.querySelectorAll('.dropdown-menu.show')).toHaveLength(1);
 });
 
 test('LinkPickerMenu should not open dropdown on click while loading', async () => {
   const { container } = render(<LinkFieldContext.Provider value={{ loading: true }}>
     <LinkPicker {...makeProps()} />
   </LinkFieldContext.Provider>);
-  await act(async () => {
-    await fireEvent.click(container.querySelector('button.link-picker__menu-toggle'));
+  userEvent.click(container.querySelector('button.link-picker__menu-toggle'));
+  await waitFor(() => {
+    expect(container.querySelectorAll('.dropdown-menu.show')).toHaveLength(0);
   });
-  expect(container.querySelectorAll('.dropdown-menu.show')).toHaveLength(0);
 });
--- client/src/tests/sample-test.js
+++
@@ -1,9 +0,0 @@
-/* global jest, describe, it, expect */
-/* eslint-disable */
-
-describe('sample tests', () => {
-  it('sample test', () => {
-    const css = 'sample css';
-    expect(css).toBe('sample css');
-  });
-});
