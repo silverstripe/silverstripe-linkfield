4971a3c4f59900d2931e732c6b93403a27a49ca1
Sabina Talipova <sabina.talipova@silverstripe.com>
2024-03-20 13:25:43 +1300

    NEW LinkableMigrationTask for migrating sheadawson-linkable links

---
+++ tests/php/Tasks/LinkableMigrationTask/HasOneLinkableLinkOwner.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace SilverStripe\LinkField\Tests\Tasks\LinkableMigrationTaskTest;
+
+use SilverStripe\Dev\TestOnly;
+use SilverStripe\ORM\DataObject;
+
+class HasOneLinkableLinkOwner extends DataObject implements TestOnly
+{
+    private static string $table_name = 'Linkable_Test_HasOneLinkableLinkOwner';
+
+    private static array $has_one = [
+        'Link' => CustomLinkableLink::class,
+    ];
+}
